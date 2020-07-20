8e8a38eb5aa3e303e0630f3d1392d07667274493
dorchen2020 <you@example.comdorchen2020@gmail.com>
2020-07-21 00:02:20 +0300

    Updates

---
+++ precache-manifest.171c7b9557b323e41451e4d0c00d074e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11dfd16640bdee01cb99e5229bcf0744",
+    "url": "/ShoppingApp/index.html"
+  },
+  {
+    "revision": "65ff4babc9faf3a2eb66",
+    "url": "/ShoppingApp/static/css/main.0794ed40.chunk.css"
+  },
+  {
+    "revision": "6d7fae9b1db49a00a9c2",
+    "url": "/ShoppingApp/static/js/2.4c1e4794.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingApp/static/js/2.4c1e4794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ff4babc9faf3a2eb66",
+    "url": "/ShoppingApp/static/js/main.3f5156fe.chunk.js"
+  },
+  {
+    "revision": "7b3f068620c3793d5fdb",
+    "url": "/ShoppingApp/static/js/runtime-main.0de63f4d.js"
+  }
+]);
